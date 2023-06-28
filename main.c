015fb751dcbc27836e033f6fc77045dabc039b1a
DBMJMarcus <jonathandaha47@gmail.com>
2023-06-28 11:04:51 +0100

    ajout de la fontion de recherche d'étudiant

--- main.c
+++ main.c
@@ -33,22 +33,48 @@ int main() {
     FILE *file = fopen("temp.txt", "a"); // Ouvre le fichier en mode ajout le fichier doit exister au préalable
     
     if (file != NULL) {
-        fprintf(file, "Temps d'exécution pour %d Etudiant : %f secondes\n", n,cpu_time_used);
+        fprintf(file, "Temps d'exécution pour l'insertion de %d Etudiant : %f secondes\n", n,cpu_time_used);
         fclose(file); // Ferme le fichier
         printf("Le temps d'exécution a été enregistré dans le fichier 'temp.txt'.\n");
     } else {
         printf("Erreur lors de l'ouverture du fichier.\n");
     }
 
-    int matricule_recherche = 50;
-    Etudiant *resultats = rechercher_etudiants_tableau(tableau_etudiant, n, matricule_recherche);
+    int N = n;
+    int matricule_recherche = rand() % (4 * N + 1) - (2 * N); // Matricule aléatoire dans [-2N; 2N]
+
+    printf("Matricule recherché : %d\n", matricule_recherche);
+
+    int nbre_etd_recherches;
+   
+
+    start = clock(); // Enregistre le temps de début
+
+    // Votre instruction à mesurer ici
+     Etudiant* etudiants_recherches = rechercher_etudiants(tableau_etudiant, n, matricule_recherche, &nbre_etd_recherches);
 
 
-    int nbre_resultats = sizeof(resultats) / sizeof(Etudiant);
-    printf("Résultats de recherche pour le matricule %d :\n", matricule_recherche);
-    for (int i = 0; i < nbre_resultats; i++) {
-        printf("Étudiant %d - Matricule: %s, Moyenne: %.2f\n", i + 1, resultats[i].nom, resultats[i].moyenne);
+    end = clock(); // Enregistre le temps de fin
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    
+    file = fopen("temp.txt", "a"); // Ouvre le fichier en mode ajout le fichier doit exister au préalable
+    
+    if (file != NULL) {
+        fprintf(file, "Temps d'exécution de la recherche pour %d Etudiant : %f secondes\n", n,cpu_time_used);
+        fclose(file); // Ferme le fichier
+        printf("Le temps d'exécution de la recherche a été enregistré dans le fichier 'temp.txt'.\n");
+    } else {
+        printf("Erreur lors de l'ouverture du fichier.\n");
     }
+
+     printf("Nombre d'étudiants trouvés : %d\n", nbre_etd_recherches);
+    for (int i = 0; i < nbre_etd_recherches; i++) {
+        printf("Étudiant %d - Matricule: %s, Moyenne: %.2f\n", i+1, etudiants_recherches[i].nom, etudiants_recherches[i].moyenne);
+    }
+
+    free(etudiants_recherches);
+    free(tableau_etudiant);
 
     return 0;
 }
