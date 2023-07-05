011485dcc1b122d3722eb2f2e6d3ec18cb2b552c
SimDevFlow <123867758+SimDevFlow@users.noreply.github.com>
2023-07-05 10:06:32 +0100

    modifications

--- main.c
+++ main.c
@@ -1,81 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "struct_tableau.h"
 #include <time.h>
+#include "tri.h"
+#include "struct_tableau.h"
 
-
-
-int main() {
+int main()
+{
     // Initialiser le générateur de nombres aléatoires avec le temps actuel
-    
-    int n;
 
     Etudiant *tableau_etudiant;
-    printf("Entrez le nombre d'étudiant : ");
-    scanf("%d",&n);
-    tableau_etudiant = malloc(sizeof(Etudiant)*n);
-    
+    int nombre_etudiant = 1000000;
+    FILE *file = fopen("temp.txt", "a"); // Ouvre le fichier en mode ajout le fichier doit exister au préalable
+
     clock_t start, end;
     double cpu_time_used;
 
-    start = clock(); // Enregistre le temps de début
+    tableau_etudiant = malloc(sizeof(Etudiant) * nombre_etudiant);
+    insertion_tableau(tableau_etudiant, nombre_etudiant);
 
-    // Votre instruction à mesurer ici
-    // Exemple : ici je met ma fonction
-    insertion_tableau(tableau_etudiant,n);
-
-
-    end = clock(); // Enregistre le temps de fin
-
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    
-    FILE *file = fopen("temp.txt", "a"); // Ouvre le fichier en mode ajout le fichier doit exister au préalable
-    
-    if (file != NULL) {
-        fprintf(file, "Temps d'exécution pour l'insertion de %d Etudiant : %f secondes\n", n,cpu_time_used);
-        fclose(file); // Ferme le fichier
-        printf("Le temps d'exécution a été enregistré dans le fichier 'temp.txt'.\n");
-    } else {
-        printf("Erreur lors de l'ouverture du fichier.\n");
-    }
-
-    int N = n;
-    int matricule_recherche = rand() % (4 * N + 1) - (2 * N); // Matricule aléatoire dans [-2N; 2N]
-
-    printf("Matricule recherché : %d\n", matricule_recherche);
-
-    int nbre_etd_recherches;
-   
+    fprintf(file, "N = %d \n", nombre_etudiant);
+    fprintf(file, "Partie tri bulle\n");
 
     start = clock(); // Enregistre le temps de début
 
-    // Votre instruction à mesurer ici
-     Etudiant* etudiants_recherches = rechercher_etudiants(tableau_etudiant, n, matricule_recherche, &nbre_etd_recherches);
-
+    tri_bulle(tableau_etudiant, nombre_etudiant);
 
     end = clock(); // Enregistre le temps de fin
 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    
-    file = fopen("temp.txt", "a"); // Ouvre le fichier en mode ajout le fichier doit exister au préalable
-    
-    if (file != NULL) {
-        fprintf(file, "Temps d'exécution de la recherche pour %d Etudiant : %f secondes\n", n,cpu_time_used);
-        fclose(file); // Ferme le fichier
-        printf("Le temps d'exécution de la recherche a été enregistré dans le fichier 'temp.txt'.\n");
-    } else {
-        printf("Erreur lors de l'ouverture du fichier.\n");
-    }
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-     printf("Nombre d'étudiants trouvés : %d\n", nbre_etd_recherches);
-    for (int i = 0; i < nbre_etd_recherches; i++) {
-        printf("Étudiant %d - Matricule: %s, Moyenne: %.2f\n", i+1, etudiants_recherches[i].nom, etudiants_recherches[i].moyenne);
-    }
+    fprintf(file, "Temps de tri de %d Etudiant : %f secondes\n", nombre_etudiant, cpu_time_used);
 
-    free(etudiants_recherches);
+    fprintf(file, "Partie tri insertion\n");
+    start = clock(); // Enregistre le temps de début
+
+    tri_insertion(tableau_etudiant, nombre_etudiant);
+
+    end = clock(); // Enregistre le temps de fin
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    fprintf(file, "Temps de tri de %d Etudiant : %f secondes\n", nombre_etudiant, cpu_time_used);
+
+    fprintf(file, "Partie tri selection\n");
+    start = clock(); // Enregistre le temps de début
+
+    tri_selection(tableau_etudiant, nombre_etudiant);
+
+    end = clock(); // Enregistre le temps de fin
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    fprintf(file, "Temps de tri de %d Etudiant : %f secondes\n", nombre_etudiant, cpu_time_used);
+
+    fprintf(file, "Partie tri fusion\n");
+    start = clock(); // Enregistre le temps de début
+
+    tri_fusion(tableau_etudiant, 0, nombre_etudiant - 1);
+
+    end = clock(); // Enregistre le temps de fin
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    fprintf(file, "Temps de tri de %d Etudiant : %f secondes\n", nombre_etudiant, cpu_time_used);
     free(tableau_etudiant);
-
+    fclose(file);
     return 0;
 }
-
