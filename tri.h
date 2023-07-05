ae0f0bfdd86b0af094f01210b6ed9745306ff51e
DBMJMarcus <jonathandaha47@gmail.com>
2023-07-05 13:14:45 +0100

    Revert "R"
    This reverts commit fcdef7c1998d14ff09198c9bc8abbc055f45b987, reversing
    changes made to 1c580aac78dffc36d72c6659e92dd544cb424cc9.

--- tri.h
+++
@@ -1,7 +0,0 @@
-#include "struct_etd.h"
-
-void tri_bulle(Etudiant *tableau_etudiant,int taille_tableau);
-void tri_insertion(Etudiant *tableau_etudiant,int taille);
-void tri_selection(Etudiant *tableau_etudiant, int taille);
-void tri_fusion(Etudiant *tab, int debut, int fin);
-void fusion(Etudiant *tab, int debut, int milieu, int fin);
