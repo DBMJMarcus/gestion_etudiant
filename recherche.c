c96e5981fbe7a142c9f357579aec0a8ed39a4320
DBMJMarcus <jonathandaha47@gmail.com>
2023-07-06 13:17:49 +0100

    Ajout de la fonction recherche dychotomique

--- recherche.c
+++ recherche.c
@@ -45,3 +45,44 @@ bool recherche_standard(Etudiant* tab_etd, int nbre_etd, int matricule_recherche
 
     return false;
 }
+
+bool recherche_dichotomique(Etudiant* tab_etd, int nbre_etd, int matricule_recherche, int* nbre_etd_recherches) {
+    int nbre_etd_recherches_max = nbre_etd * 0.2; // 20% du nombre total d'étudiants
+
+    *nbre_etd_recherches = 0;
+
+    int debut = 0;
+    int fin = nbre_etd - 1;
+    int milieu;
+
+    while (debut <= fin) {
+        milieu = (debut + fin) / 2;
+
+        if (tab_etd[milieu].matricule == matricule_recherche) {
+            // Recherché trouvé
+            int i = milieu;
+
+            while (i >= 0 && tab_etd[i].matricule == matricule_recherche) {
+                (*nbre_etd_recherches)++;
+                i--;
+            }
+
+            i = milieu + 1;
+
+            while (i < nbre_etd && tab_etd[i].matricule == matricule_recherche) {
+                (*nbre_etd_recherches)++;
+                i++;
+            }
+
+            return true;
+        }
+        else if (tab_etd[milieu].matricule < matricule_recherche) {
+            debut = milieu + 1;
+        }
+        else {
+            fin = milieu - 1;
+        }
+    }
+
+    return false;
+}
--- recherche.h
+++ recherche.h
@@ -4,5 +4,6 @@
 #include <stdbool.h>
 
 bool recherche_standard(Etudiant* tab_etd, int nbre_etd, int matricule_recherche, int* nbre_etd_recherches);
+bool recherche_dichotomique(Etudiant* tab_etd, int nbre_etd, int matricule_recherche, int* nbre_etd_recherches) {
 
 #endif
