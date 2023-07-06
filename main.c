818b5300eeb458f3ed52351f1c3c70532c6622a4
DBMJMarcus <jonathandaha47@gmail.com>
2023-07-06 13:15:27 +0100

    Ajout de la fonction recherche dychotomique

--- main.c
+++ main.c
@@ -70,6 +70,58 @@ int main() {
     
     }
  
+    fprintf(file, "\nRECHERCHE DYCHOTOMIQUE  \n");
+
+    for (int i = 0; i < 7; i++)
+    {
+        /* code */
+        
+        n = tab_valeur [i];
+        
+
+        tableau_etudiant = malloc(sizeof(Etudiant)*n);
+    
+    clock_t start, end;
+    double cpu_time_used;
+
+// deja existant por les tableaux
+
+    start = clock(); // Enregistre le temps de début
+
+    // Votre instruction à mesurer ici
+    // Exemple : ici je met ma fonction
+    insertion_tableau(tableau_etudiant,n);
+
+    end = clock(); // Enregistre le temps de fin
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+//////////////////////////////////////////////////////////////  
+    
+    int N = n;
+    int matricule_recherche = rand() % (4 * N + 1) - (2 * N);
+
+    int nbre_etd_recherches;
+    start = clock(); // Enregistre le temps de début
+
+    bool trouve = recherche_dichotomique(tableau_etudiant, N, matricule_recherche, &nbre_etd_recherches);
+
+    end = clock(); // Enregistre le temps de fin
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    fprintf(file, "Temps de Recherche pour N = %d Etudiant : %f secondes\n", n, cpu_time_used);
+
+        // if (trouve) {
+        //     printf("Nombre d'étudiants trouvés : %d\n", nbre_etd_recherches);
+        // } else {
+        //     printf("Aucun étudiant trouvé.\n");
+        // }
+
+
+    free(tableau_etudiant);
+    
+    }
+
 fclose(file);
     return 0;
 }
